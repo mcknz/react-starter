077238e8c18165ae40b045e0a09df0aa0ad1d95c
Jeff McKenzie <mail@mcknz.com>
2017-02-06 18:34:27 -0500

    added examples to for-of

--- tutorial/es6/12-forof.js
+++ tutorial/es6/12-forof.js
@@ -3,3 +3,15 @@ let list = [1,2,3,4,5];
 for (let value of list) {
   console.log(value);
 }
+
+let list2 = ['a','b','c','d','e'];
+
+for (let value2 of list2) {
+  console.log(value2);
+}
+
+let s = "word";
+
+for (let value of s) {
+  console.log(value);
+}
