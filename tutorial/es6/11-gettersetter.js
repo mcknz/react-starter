b88e945e5305c0e3aeef31a4e27b0de620673a97
Jeff McKenzie <mail@mcknz.com>
2017-02-06 18:22:02 -0500

    added new class with getters/setters

--- tutorial/es6/11-gettersetter.js
+++ tutorial/es6/11-gettersetter.js
@@ -14,3 +14,29 @@ let a = new Hello();
 a.name = "scott";
 a.hello();
 console.log(a.name);
+
+console.log(a._name);
+
+class Nums {
+  get operand1() {
+    return this._op1;
+  }
+  get operand2() {
+    return this._op2;
+  }
+  set operand1(o) {
+    this._op1 = o;
+  }
+  set operand2(o) {
+    this._op2 = o;
+  }
+  add() {
+    console.log(this._op1 + this._op2);
+  }
+}
+
+let b = new Nums
+b.operand1 = 7;
+b.operand2 = 6;
+b.add();
+
