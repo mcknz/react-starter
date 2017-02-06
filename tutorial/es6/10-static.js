8fda4fa19d6d04e4103ccf56cd82db7bbb7d3edf
Jeff McKenzie <mail@mcknz.com>
2017-02-06 18:08:30 -0500

    added custom class with static and instance methods

--- tutorial/es6/10-static.js
+++ tutorial/es6/10-static.js
@@ -8,3 +8,26 @@ class Hello {
 }
 
 Hello.world();
+
+//let b = new Hello();
+//b.world(); //this doesn't work with static, not an instance method
+
+class Nums {
+  constructor(x, y) {
+    this._x = x;
+    this._y = y;
+  }
+  add() {
+    console.log(this._x + this._y);
+  }
+  static customAdd(x, y) {
+    console.log(x+y);
+  }  
+}
+
+let b = new Nums(4, 4);
+b.add();
+Nums.customAdd(2, 3);
+console.log(b._x); // not really private
+
+
